8f89a4da88ae860b4fa8ebe344a76ce7b8b16d9b
ugurduyar <ismailugurduyar@gmail.com>
2020-06-09 16:43:05 +0300

    bugfix

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ for (let i = 0; i < brickRowCount; i++) {
   bricks[i] = [];
   for (let j = 0; j < brickColumnCount; j++) {
     const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
-    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsety;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
     bricks[i][j] = { x, y, ...brickInfo };
   }
 }
